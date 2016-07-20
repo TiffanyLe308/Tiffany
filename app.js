2eb956aaba182ca16c726bc0b674e55cdf00c1bd
Tiffany <tiffanyle308@gmail.com>
2016-07-20 23:03:57 +0300

    new tabs

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
 (function() {
+  console.log('abc');
   var app = angular.module('myApp', []);
 
-  app.controller('TabController', function() {
+  app.controller('TabController', function(){
     this.tab = 1;
-    this.selectedTab = function(setTab) {
-      this.tab = setTab;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
     };
-    this.isSet = function(tabName) {
-      return this.tab = tabName;
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
     };
   });
 
   app.directive('header', function() {
     return {
       restrict: 'A',
-      templateUrl: 'header/header.html'
-    };
-  });
-  
-  app.directive('footer', function () {
-    return {
-      restrict: 'A',
-      templateUrl: 'footer/footer.html'
+      templateUrl: 'html/header.html'
     };
   });
 
+  app.directive('introduction', function() {
+    return {
+      restrict: 'A',
+      templateUrl: 'html/introduction.html'
+    };
+  });
+
+  app.directive('footer', function () {
+    return {
+      restrict: 'A',
+      templateUrl: 'html/footer.html'
+    };
+  });
+
+  app.config(['$stateProvider', function($stateProvider) {
+    $stateProvider
+      .state('home',
+        url: '/home',
+        templateUrl: 'home.html'
+      )
+  }]);
+
+  // app.config(['$stateProvider', function($stateProvider) {
+  //   $stateProvider
+  //     .state('home',
+  //       url: '/home',
+  //       templateUrl: 'home.html'
+  //     )
+  //     .state('myblog',
+  //       url: '/myblog',
+  //       templateUrl: 'myblog.html'
+  //     )
+  //     .state('contact',
+  //       url: '/contact',
+  //       templateUrl: 'contact.html'
+  //     );
+  // }]);
 
 })();
